49b04725ff8ad6fbab309e286ff2c791f2beaa82
samplegit <samplesmtptest@gmail.com>
2024-12-12 14:37:58 +0900

    Update HelloGitWorld.java
    없음

--- HelloGitWorld.java
+++ HelloGitWorld.java
@@ -2,5 +2,6 @@ public class HelloGitWorld{
 	public static void main(String[] args){
 		System.out.println("Git Test");
 		System.out.println("Git Test222222");
+		System.out.println("Git Test333333");
 	}
 }
